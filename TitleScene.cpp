4778aaf1e3ece64e8c44c10d55ba5f6f19e858d0
Ruan Montelo <ruanmontelo@gmail.com>
2023-07-08 11:09:05 -0300

    Added a new text to the title scene so the player knows how to set the game in fullscreen

--- TitleScene.cpp
+++ TitleScene.cpp
@@ -17,15 +17,24 @@ void TitleScene::Update()
 
 	const char* enterToBeginInstructionText = "Pressione enter ou clique para iniciar!";
 
+	const char* pressFInstructionText = "Pressione a tecla F para deixar o jogo em tela cheia.";
+
 	Vector2 titleTextSize = MeasureTextEx(font, titleText, fontSize, fontSpacing);
 
 	Vector2 enterToBeginInstructionTextSize = MeasureTextEx(font, enterToBeginInstructionText, fontSize, fontSpacing);
+
+	Vector2 pressFInstructionTextSize = MeasureTextEx(font, pressFInstructionText, fontSize - 24, fontSpacing);
 
 	Vector2 titleTextPos = { (Constants::DEFAULT_WIDTH / 2) - (titleTextSize.x / 2), (Constants::DEFAULT_HEIGHT / 2) - (titleTextSize.y / 2) };
 
 	Vector2 enterToBeginInstructionTextPos = { 
 	  (Constants::DEFAULT_WIDTH / 2) - (enterToBeginInstructionTextSize.x / 2),
 	  (Constants::DEFAULT_HEIGHT / 2) + (enterToBeginInstructionTextSize.y / 2)
+	};
+
+	Vector2 pressFInstructionTextPos = {
+	  (Constants::DEFAULT_WIDTH / 2) - (pressFInstructionTextSize.x / 2),
+	  (Constants::DEFAULT_HEIGHT / 2) + (pressFInstructionTextSize.y / 2) + 100.0f
 	};
 
 	DrawTextEx(font,
@@ -39,6 +48,13 @@ void TitleScene::Update()
 		enterToBeginInstructionText,
 		enterToBeginInstructionTextPos,
 		fontSize,
+		fontSpacing,
+		BLACK);
+
+	DrawTextEx(font,
+		pressFInstructionText,
+		pressFInstructionTextPos,
+		fontSize - 24,
 		fontSpacing,
 		BLACK);
 }
